7102ae3cca8077a53634e6f16be29e7067ece750
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 15:43:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,18 +14,6 @@ end
   
   
   
-#   def reverse_each_word(string)
-#   new_sentence = string.split(" ")
-#   new_sentence
-# end
-  
-  
-  
-  # string.split(" ").each do |word|
-  
-  #     new_sentence << word.reverse
-  
-  
   
   
   
